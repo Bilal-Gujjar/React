a098e51acaac74e29438313aa39908734a44a863
Bilal-Gujjar <64352435+Bilal-Gujjar@users.noreply.github.com>
2021-02-04 08:13:35 +0500

    frim update

---
+++ hameediconsultancy/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Welcome to Hameedi Consultancy</h1>
+    <h1>Currently Site is Under Maintenance You Can Contact By Email or Whattsapp </h1>
+    <a href = "hameediab@gmail.com">Email</a>
+    <h3>Whattsapp : +92 332 8100688</h3>
+     </div>
+  );
+}
+
+export default App;
